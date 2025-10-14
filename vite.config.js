5afdb9ef1771808e0f62261aa506fdc783a6a6a4
ErnessOfficial <entrepalabrasurgentes@gmail.com>
2025-10-14 14:43:52 +0100

    Update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Use relative paths so the site works under GitHub Pages project subpath
-  base: './',
+  base: '/ernessofficial-site/',
 });
